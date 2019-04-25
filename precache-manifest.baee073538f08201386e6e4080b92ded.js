a1b5e3f0a5e7710b7d8b47786930ce77408e91cc
Gleb Rezoun <gleb.rezun@gmail.com>
2019-04-25 19:13:43 +0300

    Updates

---
+++ precache-manifest.baee073538f08201386e6e4080b92ded.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "581378ec9b6b62078f627a9cda7788a9",
+    "url": "/React-Portfolio/static/media/workspace.581378ec.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "08df8c98356f61dfc9f966325e62f03f",
+    "url": "/React-Portfolio/static/media/landscape1.08df8c98.jpg"
+  },
+  {
+    "revision": "cf5eb077fe729fdf8712cd166a01da47",
+    "url": "/React-Portfolio/static/media/headerIcon.cf5eb077.png"
+  },
+  {
+    "revision": "804d233a10add83efa8d",
+    "url": "/React-Portfolio/static/js/runtime~main.804d233a.js"
+  },
+  {
+    "revision": "7b62fd7a6c6c561c80e0",
+    "url": "/React-Portfolio/static/js/main.7b62fd7a.chunk.js"
+  },
+  {
+    "revision": "ac9956a7edd9c97010be",
+    "url": "/React-Portfolio/static/js/1.ac9956a7.chunk.js"
+  },
+  {
+    "revision": "7b62fd7a6c6c561c80e0",
+    "url": "/React-Portfolio/static/css/main.e4abafb2.chunk.css"
+  },
+  {
+    "revision": "e22a219c15302493d267b39328369940",
+    "url": "/React-Portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React-Portfolio/precache-manifest.5efb39a0c57b2eba9842f7f606f5614a.js"
+  "/React-Portfolio/precache-manifest.baee073538f08201386e6e4080b92ded.js"
 );
 
 workbox.clientsClaim();
